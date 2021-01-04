23b29b3b1f099f5d1739fdd2b851eff6e461780e
gangeshberi <gangeshberi@gmail.com>
2021-01-04 16:38:37 -0500

    modularizing code.

--- scripts/primary_tables.py
+++ scripts/primary_tables.py
@@ -4,62 +4,7 @@ import pandas as pd
 import numpy as np
 import os
 from typing import List, NoReturn
-from utils import get_project_root
-
-
-def read_data_in_data_frame(file: str) -> pd.DataFrame:
-    """
-        This function takes the file path as an input and
-        based on the type of the file either excel or csv
-        return a dataframe.
-
-        Arguments:
-            file (str): The name of the file.
-
-        Returns:
-            DataFrame: returns a dataframe created from the input file.
-    """
-    if 'xlsx' in file:
-        return pd.read_excel(file, engine='openpyxl')
-    elif 'csv' in file:
-        return pd.read_csv(file)
-    else:
-        raise ValueError(
-            'Invalid Argument to the read_data_in_data_frame function!')
-
-
-def concat_data_frame(files: List[str]) -> pd.DataFrame:
-    """
-        This function takes the list of the input files
-        and returns the concatenated dataframe that is created
-        using the input files.
-
-        Arguments:
-            files (list of str): List of the input files.
-
-        Returns:
-            DataFrame: returns a concatenated dataframe.
-    """
-    data_frames = []
-    for file in files:
-        data_frames.append(read_data_in_data_frame(file))
-    return pd.concat(data_frames)
-
-
-def write_df_to_csv(data_frame: pd.DataFrame, path: str, label: str) -> NoReturn:
-    """
-        This function write the dataframe to the give path
-        and assigns the index label to the value passed in the parameter.
-
-        Arguments:
-            data_frame (pandas dataframe): Data Frame that will be written to the file.
-            path (str): The path of the output CSV file.
-            label (str): Index label for .to_csv function in pandas.
-
-        Returns:
-            The function doesn't return anything.
-    """
-    data_frame.to_csv(path, index_label=label)
+from utils import get_project_root, read_data_in_data_frame, concat_data_frame, write_df_to_csv
 
 
 print(" <----------------------------------------- (Start Building Drug Table) ----------------------------------------> \n")
--- scripts/utils.py
+++ scripts/utils.py
@@ -1,8 +1,65 @@
 from pathlib import Path
+from typing import List, NoReturn
+import pandas as pd
 
 
-"""
-    @returns the root path of the project.
-"""
 def get_project_root() -> Path:
+    """
+        @returns the root path of the project.
+    """
     return Path(__file__).parent.parent.absolute()
+
+
+def read_data_in_data_frame(file: str) -> pd.DataFrame:
+    """
+        This function takes the file path as an input and
+        based on the type of the file either excel or csv
+        return a dataframe.
+
+        Arguments:
+            file (str): The name of the file.
+
+        Returns:
+            DataFrame: returns a dataframe created from the input file.
+    """
+    if 'xlsx' in file:
+        return pd.read_excel(file, engine='openpyxl')
+    elif 'csv' in file:
+        return pd.read_csv(file)
+    else:
+        raise ValueError(
+            'Invalid Argument to the read_data_in_data_frame function!')
+
+
+def concat_data_frame(files: List[str]) -> pd.DataFrame:
+    """
+        This function takes the list of the input files
+        and returns the concatenated dataframe that is created
+        using the input files.
+
+        Arguments:
+            files (list of str): List of the input files.
+
+        Returns:
+            DataFrame: returns a concatenated dataframe.
+    """
+    data_frames = []
+    for file in files:
+        data_frames.append(read_data_in_data_frame(file))
+    return pd.concat(data_frames)
+
+
+def write_df_to_csv(data_frame: pd.DataFrame, path: str, label: str) -> NoReturn:
+    """
+        This function write the dataframe to the give path
+        and assigns the index label to the value passed in the parameter.
+
+        Arguments:
+            data_frame (pandas dataframe): Data Frame that will be written to the file.
+            path (str): The path of the output CSV file.
+            label (str): Index label for .to_csv function in pandas.
+
+        Returns:
+            The function doesn't return anything.
+    """
+    data_frame.to_csv(path, index_label=label)
