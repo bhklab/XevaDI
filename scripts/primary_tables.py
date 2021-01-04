b47129060dd33ecdfbfdef738d121ed2c8e57cb2
gangeshberi <gangeshberi@gmail.com>
2021-01-04 16:33:56 -0500

    added code for `datasets` code to primary tables.

--- scripts/primary_tables.py
+++ scripts/primary_tables.py
@@ -1,6 +1,7 @@
 import glob
 import pathlib
 import pandas as pd
+import numpy as np
 import os
 from typing import List, NoReturn
 from utils import get_project_root
@@ -61,8 +62,7 @@ def write_df_to_csv(data_frame: pd.DataFrame, path: str, label: str) -> NoReturn
     data_frame.to_csv(path, index_label=label)
 
 
-print(" <----------------------------------------- Started Building Drug Table -----------------------------------------> ")
-
+print(" <----------------------------------------- (Start Building Drug Table) ----------------------------------------> \n")
 # input files to read and output file path.
 input_files = glob.glob('../input_data/*/drug_screening.*')
 output_file = '../output_data/drugs.csv'
@@ -74,11 +74,27 @@ drug_input_df = concat_data_frame(input_files)
 unique_drugs = drug_input_df['drug'].str.upper().unique()
 
 # create the drug data frame.
-drug_df = pd.DataFrame(unique_drugs, columns=['drug_name'])
-drug_df = drug_df.rename_axis('drug_id', axis=1)
-drug_df.index += 1
+index = np.arange(1, len(unique_drugs) + 1)
+drug_df = pd.DataFrame(unique_drugs, columns=['drug_name'], index=index)
 
 # write data/dataframe to the csv file.
 write_df_to_csv(drug_df, output_file, 'drug_id')
+print(" <----------------------------------------- (Done Building Drug Table) ------------------------------------------> \n")
 
-print(" <----------------------------------------- Done Building Drug Table ------------------------------------------> ")
+
+print(" <----------------------------------------- (Start Building Dataset Table) ----------------------------------------> \n")
+# output file.
+output_file = '../output_data/datasets.csv'
+
+# dataset list.
+datasets = ['PDXE(Breast Cancer)', 'PDXE(Colorectal Cancer)', 'PDXE(Cutaneous Melanoma)',
+            'PDXE(Gastric Cancer)', 'PDXE(Non-small Cell Lung Carcinoma)', 'PDXE(Pancreatic Ductal Carcinoma)',
+            'UHN(Breast Cancer)', 'McGill(Breast Cancer)']
+
+# create dataset series.
+index = np.arange(1, len(datasets) + 1)
+dataset_series = pd.Series(datasets, name='dataset_name', index=index)
+
+# write data to the csv file.
+write_df_to_csv(dataset_series, output_file, 'dataset_id')
+print(" <----------------------------------------- (Done Building Dataset Table) ----------------------------------------> \n")
