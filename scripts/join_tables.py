4f861dc7e8ad27921dad1cde15c83bc3bc31c94f
gangeshberi <gangeshberi@gmail.com>
2021-01-19 14:51:26 -0500

    `drug_screening` and `model_response` tables in join_tables.py.

--- scripts/join_tables.py
+++ scripts/join_tables.py
@@ -77,12 +77,91 @@ def batch_information_table(path: str) -> NoReturn:
         batch_information_output_file, 'id')
 
 
+def model_response_table(path: str) -> NoReturn:
+    """
+    This function creates the data frame from the input files, concatenates them
+    and write it to the csv file.
+
+    Arguments:a
+        path(str): absolute path to the parent's parent directory.
+    """
+
+    # comment
+    comment('model_response')
+
+    # input files for model_response and drug file path and output file path.
+    input_files = glob.glob(f'{path}/input_data/*/model_response.*')
+    drug_file = f'{path}/output_data/drugs.csv'
+    model_file = f'{path}/output_data/models.csv'
+    model_response_output_file = f'{path}/output_data/model_response.csv'
+
+    # concatenated data frame.
+    model_response_input_df = concat_data_frame(input_files)
+
+    # drug and model data frame.
+    drug_df = read_data_in_data_frame(drug_file)
+    model_df = read_data_in_data_frame(model_file)
+
+    # merging the drug df and model df.
+    merged_df = model_response_input_df.merge(
+        drug_df, left_on='drug', right_on='drug_name').merge(model_df, left_on='model.id', right_on='model')
+
+    merged_df.index = np.arange(1, len(merged_df) + 1)
+
+    # writing the modified df to the csv file for model_response table.
+    write_data_to_csv(
+        merged_df[['drug_id', 'model_id', 'response_type', 'value']],
+        model_response_output_file, 'id')
+
+
+def drug_screening_table(path: str) -> NoReturn:
+    """
+    This function creates the data frame from the input files, concatenates them
+    and write it to the csv file.
+
+    Arguments:a
+        path(str): absolute path to the parent's parent directory.
+    """
+
+    # comment
+    comment('drug_screening')
+
+    # input files for drug_screening and drug file path and output file path.
+    input_files = glob.glob(f'{path}/input_data/*/drug_screening.*')
+    drug_file = f'{path}/output_data/drugs.csv'
+    model_file = f'{path}/output_data/models.csv'
+    drug_screening_output_file = f'{path}/output_data/drug_screening.csv'
+
+    # concatenated data frame.
+    drug_screening_input_df = concat_data_frame(input_files)
+
+    # drug and model data frame.
+    drug_df = read_data_in_data_frame(drug_file)
+    model_df = read_data_in_data_frame(model_file)
+
+    # merging the drug df and model df.
+    merged_df = drug_screening_input_df.merge(
+        drug_df, left_on='drug', right_on='drug_name').merge(model_df, left_on='model.id', right_on='model')
+
+    merged_df.index = np.arange(1, len(merged_df) + 1)
+
+    # renaming column in the dataframe.
+    merged_df.rename(columns={'volume.normal': 'volume_normal'}, inplace=True)
+
+    # writing the modified df to the csv file for drug_screening table.
+    write_data_to_csv(
+        merged_df[['drug_id', 'model_id', 'time', 'volume', 'volume_normal']],
+        drug_screening_output_file, 'id')
+
+
 def build_join_tables() -> NoReturn:
     # get the path of the root directory.
     project_path = f'{get_project_root()}'
 
-    batch_response_table(project_path)
-    batch_information_table(project_path)
+    # batch_response_table(project_path)
+    # batch_information_table(project_path)
+    # model_response_table(project_path)
+    drug_screening_table(project_path)
 
 
 build_join_tables()
