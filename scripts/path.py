5717b4ad7710cd90847d6f697a1c8d3ac2808083
gangeshberi <gangeshberi@gmail.com>
2021-02-26 10:22:56 -0500

    directly passing in the data file paths to `path.py`.

--- scripts/path.py
+++ scripts/path.py
@@ -1,5 +1,4 @@
 import glob
-from utils import get_project_root
 from typing import Dict
 
 
@@ -10,24 +9,24 @@ def get_input_files_path(path: str) -> Dict[str, list]:
     corresponding key.
 
     Argument(s):
-        path(str): absolute path to the parent's parent directory.
+        path(str): path to the input data directory.
 
     Returns:
         Dict[str, list]: dictionary of the path of the output files.
     """
     return {
-        'batch_information': glob.glob(f'{path}/input_data/*/batch_information.*'),
-        'batch_response': glob.glob(f'{path}/input_data/*/batch_response.*'),
-        'copy_number_variation': glob.glob(f'{path}/input_data/*/copy_number_variation.*'),
-        'drug_screening': glob.glob(f'{path}/input_data/*/drug_screening.*'),
-        'model_information': glob.glob(f'{path}/input_data/*/model_information.*'),
-        'model_response': glob.glob(f'{path}/input_data/*/model_response.*'),
-        'modelid_moleculardata_mapping': glob.glob(f'{path}/input_data/*/modelid_moleculardata_mapping.*'),
-        'mutation': glob.glob(f'{path}/input_data/*/mutation.*'),
-        'rna_sequencing': glob.glob(f'{path}/input_data/*/rna_sequencing.*'),
-        'model_sheet': f'{path}/input_data/model_sheets.csv',
-        'drug_annotation': f'{path}/input_data/drug_annotations.csv',
-        'pubchem_annotation': f'{path}/input_data/drug_annotations_1.csv',
+        'batch_information': glob.glob(f'{path}/*/batch_information.*'),
+        'batch_response': glob.glob(f'{path}/*/batch_response.*'),
+        'copy_number_variation': glob.glob(f'{path}/*/copy_number_variation.*'),
+        'drug_screening': glob.glob(f'{path}/*/drug_screening.*'),
+        'model_information': glob.glob(f'{path}/*/model_information.*'),
+        'model_response': glob.glob(f'{path}/*/model_response.*'),
+        'modelid_moleculardata_mapping': glob.glob(f'{path}/*/modelid_moleculardata_mapping.*'),
+        'mutation': glob.glob(f'{path}/*/mutation.*'),
+        'rna_sequencing': glob.glob(f'{path}/*/rna_sequencing.*'),
+        'model_sheet': f'{path}/model_sheets.csv',
+        'drug_annotation': f'{path}/drug_annotations.csv',
+        'pubchem_annotation': f'{path}/drug_annotations_1.csv',
     }
 
 
@@ -36,29 +35,29 @@ def get_output_files_path(path: str) -> Dict[str, str]:
     This function creates a dictionary of the path of the output files for the database tables.
 
     Argument(s):
-        path(str): absolute path to the parent's parent directory.
+        path(str): path to the output data directory.
 
     Returns:
         Dict[str, str]: dictionary of the path of the output files.
     """
     return {
-        'drug': f'{path}/output_data/drugs.csv',
-        'model': f'{path}/output_data/models.csv',
-        'dataset': f'{path}/output_data/datasets.csv',
-        'patient': f'{path}/output_data/patients.csv',
-        'tissue': f'{path}/output_data/tissues.csv',
-        'batch': f'{path}/output_data/batches.csv',
-        'sequencing': f'{path}/output_data/sequencing.csv',
-        'gene': f'{path}/output_data/genes.csv',
-        'batch_response': f'{path}/output_data/batch_response.csv',
-        'batch_information': f'{path}/output_data/batch_information.csv',
-        'model_response': f'{path}/output_data/model_response.csv',
-        'drug_screening': f'{path}/output_data/drug_screening.csv',
-        'model_information': f'{path}/output_data/model_information.csv',
-        'mutation': f'{path}/output_data/mutation.csv',
-        'copy_number_variation': f'{path}/output_data/copy_number_variation.csv',
-        'rna_sequencing': f'{path}/output_data/rna_sequencing.csv',
-        'modelid_moleculardata_mapping': f'{path}/output_data/modelid_moleculardata_mapping.csv',
-        'model_sheet': f'{path}/output_data/model_sheets.csv',
-        'drug_annotation': f'{path}/output_data/drug_annotations.csv',
+        'drug': f'{path}/drugs.csv',
+        'model': f'{path}/models.csv',
+        'dataset': f'{path}/datasets.csv',
+        'patient': f'{path}/patients.csv',
+        'tissue': f'{path}/tissues.csv',
+        'batch': f'{path}/batches.csv',
+        'sequencing': f'{path}/sequencing.csv',
+        'gene': f'{path}/genes.csv',
+        'batch_response': f'{path}/batch_response.csv',
+        'batch_information': f'{path}/batch_information.csv',
+        'model_response': f'{path}/model_response.csv',
+        'drug_screening': f'{path}/drug_screening.csv',
+        'model_information': f'{path}/model_information.csv',
+        'mutation': f'{path}/mutation.csv',
+        'copy_number_variation': f'{path}/copy_number_variation.csv',
+        'rna_sequencing': f'{path}/rna_sequencing.csv',
+        'modelid_moleculardata_mapping': f'{path}/modelid_moleculardata_mapping.csv',
+        'model_sheet': f'{path}/model_sheets.csv',
+        'drug_annotation': f'{path}/drug_annotations.csv',
     }
