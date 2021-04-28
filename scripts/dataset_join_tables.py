5b3cfd18888d2152a78b67037b2823b3ee25f400
gangeshberi <gangeshberi@gmail.com>
2021-04-28 02:02:34 -0400

    removed `datasets_patients` table as the dataset id is included in patients table now.

--- scripts/dataset_join_tables.py
+++ scripts/dataset_join_tables.py
@@ -16,40 +16,6 @@ def model_information_df(data: Dict) -> pd.DataFrame:
     return concat_data_frame(data['model_information'], {'patient.id': str, 'dataset': str}).replace('TNBC', 'UHN (Breast Cancer)')
 
 
-def datasets_patients_table(input_files: Dict, output_files: Dict) -> NoReturn:
-    """
-    This function creates the data frame from the input files, concatenates them
-    and write it to the csv file.
-
-    Arguments:
-        output_files (dict): Contains the dictionary of the output files.
-        input_files (dict): Contains the dictionary of the input files.
-    """
-    # comment.
-    comment('datasets_patients')
-
-    # patient data frame.
-    patient_df = read_data_in_data_frame(
-        output_files['patient'], {'patient_id': int, 'patient': str})
-
-    # dataset data frame.
-    dataset_df = read_data_in_data_frame(
-        output_files['dataset'], {'dataset_id': int, 'dataset': str})
-
-    # merge model information data frame with dataset and patient data frame.
-    merged_df = model_information_df(input_files).merge(
-        patient_df, left_on='patient.id', right_on='patient').merge(
-            dataset_df, left_on='dataset', right_on='dataset_name')
-
-    # drop the duplicates and grab dataset_id and patient_id.
-    final_df = merged_df.drop_duplicates(['dataset_id', 'patient_id'])[[
-        'dataset_id', 'patient_id']].sort_values(by=['dataset_id', 'patient_id'])
-
-    # writing the modified df to the csv file for datasets_patients table.
-    write_data_to_csv(
-        final_df, output_files['dataset_patient'], data_type={'dataset_id': int, 'patient_id': int})
-
-
 def datasets_tissues_table(input_files: Dict, output_files: Dict) -> NoReturn:
     """
     This function creates the data frame from the input files, concatenates them
