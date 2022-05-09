8d3a6c8296d97450b5a4be2289ca8417426a56a2
void <gangeshberi1@gmail.com>
2022-05-09 13:41:49 -0400

    Update: `mappers` file

--- scripts/mappers.py
+++ scripts/mappers.py
@@ -1,11 +1,19 @@
 # dataset mapper object
 # to map dataset names in the biomarker data to actual dataset names in the database
 dataset_mapping = {
-    'PDXE_Breast_Cancer_XevaSet': 'PDXE (Breast Cancer)'
+    'PDXE_Breast_Cancer_XevaSet': 'PDXE (Breast Cancer)',
+    'PDXE_Colorectal_Cancer_XevaSet': 'PDXE (Colorectal Cancer)',
+    'PDXE_Cutaneous_Melanoma_XevaSet': 'PDXE (Cutaneous Melanoma)',
+    'PDXE_Non-small_Cell_Lung_Carcinoma_XevaSet': 'PDXE (Non-small Cell Lung Carcinoma)',
+    'PDXE_Pancreatic_Ductal_Carcinoma_XevaSet': 'PDXE (Pancreatic Ductal Carcinoma)',
 }
 
 # tissue mapper object
 # to map tissue names in the biomarker data to actual tissue names in the database
 tissue_mapping = {
-    'breast': 'Breast Cancer'
+    'breast': 'Breast Cancer',
+    'colorectal cancer': 'Colorectal Cancer',
+    'melanoma': 'Cutaneous Melanoma',
+    'lung': 'Non-small Cell Lung Carcinoma',
+    'pancreatic cancer': 'Pancreatic Ductal Carcinoma',
 }
