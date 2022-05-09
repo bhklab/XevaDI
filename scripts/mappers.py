c6d1ff7f79dba8a2c449331558cfa95fd206c4e3
void <gangeshberi1@gmail.com>
2022-05-09 13:01:30 -0400

    Add: new mapper file to include `tissue` and `dataset` mapper objects

---
+++ scripts/mappers.py
@@ -0,0 +1,11 @@
+# dataset mapper object
+# to map dataset names in the biomarker data to actual dataset names in the database
+dataset_mapping = {
+    'PDXE_Breast_Cancer_XevaSet': 'PDXE (Breast Cancer)'
+}
+
+# tissue mapper object
+# to map tissue names in the biomarker data to actual tissue names in the database
+tissue_mapping = {
+    'breast': 'Breast Cancer'
+}
